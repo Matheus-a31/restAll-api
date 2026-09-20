cb299e74ff315066aaa85e5d0b5e76d64828907d
Matheus-a31 <matheusassis3103@gmail.com>
2026-09-20 19:42:17 -0300

    Refactor: refatora o controller  para permitir o front end enviar a data

--- src/main/java/com/br/RestAll/dashboard/controller/DashboardController.java
+++ src/main/java/com/br/RestAll/dashboard/controller/DashboardController.java
@@ -4,11 +4,14 @@ import com.br.RestAll.dashboard.dto.DashboardResponse;
 import com.br.RestAll.dashboard.dto.PeriodoDashboard;
 import com.br.RestAll.dashboard.service.DashboardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/dashboard")
@@ -19,7 +22,10 @@ public class DashboardController {
     private final DashboardService dashboardService;
 
     @GetMapping
-    public DashboardResponse getDashboard(@RequestParam(defaultValue = "DIA") PeriodoDashboard periodo) {
-        return dashboardService.getDashboardData(periodo);
+    public DashboardResponse getDashboard(
+            @RequestParam(required = false, defaultValue = "MES") PeriodoDashboard periodo,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataInicio,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim) {
+        return dashboardService.getDashboardData(periodo, dataInicio, dataFim);
     }
 }
